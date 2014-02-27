d640339cef86c5b098bb51f1c25ad95d0648764e
Eldar Gabdullin <eldargab@gmail.com>
2014-02-27 12:26:52 +0400

    description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject easy-app/core "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Dependency injection for Clojure"
   :url "http://github.com/eldargab/easy-app.clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
