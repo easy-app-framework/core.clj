7ca0f2a67c024b6cee2c08836f1656f47d6c71eb
Eldar Gabdullin <eldargab@gmail.com>
2014-06-09 17:43:47 +0400

    implement cljs version

---
+++ src/dar/container.cljs
@@ -0,0 +1,67 @@
+(ns dar.container)
+
+(defrecord Container [fns state parent level])
+
+(defrecord Fn [fn args level])
+
+(defn start
+  ([container level vals]
+   (->Container (:fns container) (atom vals) container level))
+  ([container vals]
+   (start container nil vals))
+  ([container]
+   (start container {})))
+
+(defn stop! [container])
+
+(defn- lookup [container k]
+  (let [parent (:parent container)
+        vals @(:state container)
+        val (get vals k ::nil)]
+    (if (and parent (= ::nil val))
+      (recur parent k)
+      val)))
+
+(defn- find-level [container level]
+  (if (and level (not= level (:level container)))
+    (recur (:parent container) level)
+    container))
+
+(declare do-eval)
+
+(defn eval [this k]
+  (let [val (lookup this k)]
+    (if (= ::nil val)
+      (do-eval this k)
+      val)))
+
+(defn- do-eval [container k]
+  (let [{:keys [args fn level] :as spec} (-> container :fns (get k))
+        this (find-level container level)
+        state (get this :state)]
+
+    (when-not spec
+      (throw (Error. (str "Cell " k " is not defined"))))
+
+    (let [ret (apply fn (map #(eval this %) args))]
+      (swap! state assoc k ret)
+      ret)))
+
+;;
+;; Spec API
+;;
+
+(defn- fn-entry? [[k v]]
+  (instance? Fn v))
+
+(defn make* [spec]
+  (map->Container {:fns (into {} (filter fn-entry? spec))
+                   :state (atom (into {} (filter (complement fn-entry?) spec)))
+                   :level :app}))
+
+(defn define*
+  ([spec k v]
+   (assoc spec k v))
+  ([spec k opt-k opt-v & {:as opts}]
+   (assoc spec k (map->Fn (merge {:args []}
+                                 (assoc opts opt-k opt-v))))))
