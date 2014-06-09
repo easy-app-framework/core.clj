0ee60427a4297252f384f0423d50387bd172a39a
Eldar Gabdullin <eldargab@gmail.com>
2014-06-09 17:58:18 +0400

    depend on a released version of async

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [dar/async "0.1.0-SNAPSHOT"]]
+                 [dar/async "0.1.0"]]
   :profiles {:dev {:dependencies [[criterium "0.4.3"]]
                    :source-paths ["bench"]
                    :aliases {"bench" ["run" "-m" "dar.container.bench"]}}}
