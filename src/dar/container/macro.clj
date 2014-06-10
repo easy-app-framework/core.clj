87bfe1945a276e91b4ebd8f554cf5cd1e0fc2c73
Eldar Gabdullin <eldargab@gmail.com>
2014-06-10 14:41:37 +0400

    fix cljs dsl macro

--- src/dar/container/macro.clj
+++ src/dar/container/macro.clj
@@ -2,9 +2,10 @@
   "DSL for ClojureScript version")
 
 (defmacro define [& args]
-  `(when-not ~'*dar-container-spec*
-     (def ~'*dar-container-spec* (atom {})))
-  `(swap! ~'*dar-container-spec* #(dar.container/define* % ~@args)))
+  `(do
+     (when-not ~'*dar-container-spec*
+       (def ~'*dar-container-spec* (atom {})))
+     (swap! ~'*dar-container-spec* #(dar.container/define* % ~@args))))
 
 (defn- spec-symbol [ns-quote]
   (symbol (name (second ns-quote)) "*dar-container-spec*"))
