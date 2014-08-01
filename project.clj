4a89da5d232aea007f0740eb113cb9b16b35daec
Eldar Gabdullin <eldargab@gmail.com>
2014-08-02 00:51:39 +0400

    bump clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/dar-clojure/core"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [dar/async.promise "0.1.0"]]
   :profiles {:dev {:dependencies [[criterium "0.4.3"]]
                    :source-paths ["bench"]
